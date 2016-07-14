cd51d5259a624e2aab6d42830dab2c24ff0d53c8
Radamanthus Batnag <radamanthus@gmail.com>
2016-07-14 23:24:18 +0800

    Add docker_youtrack recipe

---
+++ cookbooks/docker_youtrack/recipes/default.rb
@@ -0,0 +1,39 @@
+['/data/youtrack/lib', '/data/youtrack/local/logs', '/data/youtrack/local/conf', '/data/youtrack/local/data', '/data/youtrack/local/backups'].each do |name|
+  directory name do
+    action :create
+    recursive true
+    owner 2000
+    group 2000
+    mode 0755
+  end
+end
+
+docker_image 'docker-youtrack' do
+  repo 'dzwicker/docker-youtrack'
+  action :pull
+end
+
+docker_container 'docker-youtrack' do
+  repo 'dzwicker/docker-youtrack'
+  port '8081:8080'
+  volumes ['/data/youtrack/lib:/var/lib/youtrack', '/data/youtrack/local/logs:/usr/local/youtrack/logs', '/data/youtrack/local/conf:/usr/local/youtrack/conf', '/data/youtrack/local/data:/usr/local/youtrack/data', '/data/youtrack/local/backups:/usr/local/youtrack/backups']
+  restart_policy 'always'
+  action :run
+  kill_after 10
+end
+
+service 'nginx' do
+  supports restart: true, reload: true, status: true
+  action :nothing
+end
+
+template '/etc/nginx/servers/youtrack.ssl.conf' do
+  owner 'deploy'
+  group 'deploy'
+  mode 0644
+  source 'youtrack.conf.ssl.erb'
+  notifies :reload, 'service[nginx]'
+  variables ({
+    server_name: 'youtrack.mydomain.com'
+  })
+end
