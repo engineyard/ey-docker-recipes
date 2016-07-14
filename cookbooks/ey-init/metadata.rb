9d6b3e7a33e0a197d02e49ef885345b815ec5c8d
Christopher Rigor <crigor@gmail.com>
2016-07-14 15:25:43 +0800

    Add ey-init

---
+++ cookbooks/ey-init/metadata.rb
@@ -0,0 +1,12 @@
+name 'ey-init'
+maintainer 'Engine Yard'
+maintainer_email 'support@engineyard.com'
+version '1.0'
+source_url 'https://engineyard.com'
+issues_url 'https://support.engineyard.com'
+
+depends 'ey-lib'
+depends 'ey-core'
+depends 'ey-base'
+
+depends 'customer_docker'
