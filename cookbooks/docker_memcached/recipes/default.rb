a02cff2cbf5402265d9ef7f68cccaba6308e09fa
Christopher Rigor <crigor@gmail.com>
2016-07-15 21:24:32 +0800

    Add memcached_docker.yml

--- cookbooks/docker_memcached/recipes/default.rb
+++ cookbooks/docker_memcached/recipes/default.rb
@@ -14,3 +14,32 @@ if is_docker_memcached_instance
     action :run
   end
 end
+
+if ['solo', 'app', 'app_master', 'util'].include?(node.dna[:instance_role])
+  instances = node.dna[:engineyard][:environment][:instances]
+  docker_instance = instances.find{|i| i[:name] == node[:docker_memcached][:utility_name]}
+
+  if docker_instance.nil?
+    raise "Docker instance named '#{node[:docker_memcached][:utility_name]}' does not exist. Please fix the docker_memcached recipe."
+  end
+
+  node.dna[:engineyard][:environment][:apps].each do |app|
+    directory "/data/#{app[:name]}/shared/config" do
+      recursive true
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0755
+    end
+
+    template "/data/#{app[:name]}/shared/config/memcached_docker.yml" do
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0644
+      source "memcached_docker.yml.erb"
+      variables({
+        :environment => node.dna[:environment][:framework_env],
+        :hostname => docker_instance[:private_hostname]
+      })
+    end
+  end
+end
