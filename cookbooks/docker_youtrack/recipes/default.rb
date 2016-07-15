c4e739679a0961ba73887fdcd000e693ae1bed7d
Radamanthus Batnag <radamanthus@gmail.com>
2016-07-15 15:12:38 +0800

    WIP: docker_youtrack refinements

--- cookbooks/docker_youtrack/recipes/default.rb
+++ cookbooks/docker_youtrack/recipes/default.rb
@@ -1,39 +1,10 @@
-['/data/youtrack/lib', '/data/youtrack/local/logs', '/data/youtrack/local/conf', '/data/youtrack/local/data', '/data/youtrack/local/backups'].each do |name|
-  directory name do
-    action :create
-    recursive true
-    owner 2000
-    group 2000
-    mode 0755
-  end
+is_youtrack_instance = (node.dna['instance_role'].eql? 'util') && (node.dna['name'].eql? 'docker_youtrack')
+if is_youtrack_instance
+  include_recipe 'customer_docker::install'
+  include_recipe 'docker_youtrack::setup_container'
 end
 
-docker_image 'docker-youtrack' do
-  repo 'dzwicker/docker-youtrack'
-  action :pull
-end
-
-docker_container 'docker-youtrack' do
-  repo 'dzwicker/docker-youtrack'
-  port '8081:8080'
-  volumes ['/data/youtrack/lib:/var/lib/youtrack', '/data/youtrack/local/logs:/usr/local/youtrack/logs', '/data/youtrack/local/conf:/usr/local/youtrack/conf', '/data/youtrack/local/data:/usr/local/youtrack/data', '/data/youtrack/local/backups:/usr/local/youtrack/backups']
-  restart_policy 'always'
-  action :run
-  kill_after 10
-end
-
-service 'nginx' do
-  supports restart: true, reload: true, status: true
-  action :nothing
-end
-
-template '/etc/nginx/servers/youtrack.ssl.conf' do
-  owner 'deploy'
-  group 'deploy'
-  mode 0644
-  source 'youtrack.conf.ssl.erb'
-  notifies :reload, 'service[nginx]'
-  variables ({
-    server_name: 'youtrack.mydomain.com'
-  })
+is_app_instance = ['solo', 'app_master', 'app'].include?(node.dna['instance_role'])
+if is_app_instance
+  include_recipe 'docker_youtrack::setup_nginx'
 end
