cfe5c52bb15baf768740576f4c5fd7a24d8a0506
Radamanthus Batnag <radamanthus@gmail.com>
2016-08-04 08:44:49 +0800

    Do not hardcode the 'docker_youtrack' instance name

--- cookbooks/docker_youtrack/recipes/default.rb
+++ cookbooks/docker_youtrack/recipes/default.rb
@@ -1,4 +1,4 @@
-is_youtrack_instance = (node.dna[:instance_role] == 'util') && (node.dna[:name] == 'docker_youtrack')
+is_youtrack_instance = (node.dna[:instance_role] == 'util') && (node.dna[:name] == node[:docker_youtrack][:utility_name])
 if is_youtrack_instance
   include_recipe 'docker_youtrack::setup_container'
 end
