216b37181e65b46f0f0735a8d65310e6b2686ee0
Allan Espinosa <aespinosa@engineyard.com>
2016-08-12 07:01:21 +0900

    Setup the database credentials like in custom v5
    Similar to
    https://github.com/engineyard/ey-cookbooks-stable-v5/blob/master/cookbooks/sequelizejs/attributes/default.rb

--- cookbooks/docker_nodejs/attributes/default.rb
+++ cookbooks/docker_nodejs/attributes/default.rb
@@ -1,2 +1,7 @@
 default['docker_nodejs']['utility_name'] = 'docker'
 default['docker_nodejs']['server_name'] = 'nodejs.example.com'
+
+default['docker_nodejs']['database'].tap do |db|
+  db['dialect'] = 'postgresql'
+  db['name'] = 'docker_nodejs'
+end
